947c1f5378e95d21a3df0c3d88649c242ed9056c
vbenavente <vbenavente@hotmail.com>
2016-03-18 09:55:31 -0700

    solved problem 2 app.js

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ function sum(numOne, numTwo){
   var total = numOne + numTwo;
   console.log('The sum of ' + numOne + ' and ' + numTwo + ' is ' + total + '.');
 }
+
+function multiply(mulOne, mulTwo) {
+  var product = mulOne * mulTwo;
+  console.log('The product of ' + mulOne + ' and ' + mulTwo + ' is ' + product + '.');
+}
