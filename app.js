8a36ffa52323bb3de81763f8b4cb2929e2a3ac73
vbenavente <vbenavente@hotmail.com>
2016-03-18 09:43:46 -0700

    forgot .

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ console.log('javascript is working');
 
 function sum(numOne, numTwo){
   var total = numOne + numTwo;
-  console.log('The sum of ' + numOne + ' and ' + numTwo + ' is ' + total);
+  console.log('The sum of ' + numOne + ' and ' + numTwo + ' is ' + total + '.');
 }
